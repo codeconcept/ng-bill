f04f61b51e64bbe9e0fa2b35fb45ef793659bbcb
codeconcept <contact@codeconcept.fr>
2018-04-23 13:24:35 +0200

    dynamically adding extra FormGroup

--- src/app/bill/bill.component.ts
+++ src/app/bill/bill.component.ts
@@ -1,27 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators, FormArray } from "@angular/forms";
 
 @Component({
-  selector: 'app-bill',
-  templateUrl: './bill.component.html',
-  styleUrls: ['./bill.component.css']
+  selector: "app-bill",
+  templateUrl: "./bill.component.html",
+  styleUrls: ["./bill.component.css"]
 })
 export class BillComponent implements OnInit {
   billForm: FormGroup;
   billNumber: number = 1;
+  articles: FormArray = null;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.billForm = this.fb.group({
       billNumber: [this.billNumber],
-      articleQuantity: [1, Validators.required],
-      articleName: ['', Validators.required]
+      articles: this.fb.array([
+        this.fb.group({
+          articleQuantity: [1, Validators.required],
+          articleName: ["", Validators.required]
+        })
+      ])
     });
+
+    this.articles = <FormArray>this.billForm.get("articles");
   }
 
-  create() {
-    
+  addExtraArticle() {
+    this.articles.push(
+      this.fb.group({
+        articleQuantity: [1, Validators.required],
+        articleName: ["", Validators.required]
+      })
+    );
+    console.log(this.billForm);
   }
 
+  create() {}
 }
